cc1517ce8170afc9a9679b3daf531e14055014a7
duanxiaoqiu <duanxiaoqiu@qiyi.com>
2021-09-07 09:51:28 +0800

    init

---
+++ src/main/java/org/example/GithubRepoPageProcessor.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import com.alibaba.fastjson.JSONObject;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GithubRepoPageProcessor implements PageProcessor {
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(10000);
+
+    public static final String URL_POST = "http://blog\\.sina\\.com\\.cn/s/blog_\\w+\\.html";
+
+
+    @Override
+    public void process(Page page) {
+
+        List<String> urls= page.getHtml().css("a.UserLink-link").all();
+        page.putField("size",urls.size());
+        page.putField("all", Arrays.toString(urls.toArray()));
+
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=======start=========");
+        Spider.create(new GithubRepoPageProcessor())
+                //从"https://github.com/code4craft"开始抓
+                .addUrl("https://www.zhihu.com/people/zhugezifang/following?page=1")
+                .addPipeline(new ConsolePipeline())
+                //开启5个线程抓取
+                .thread(5)
+                //启动爬虫
+                .run();
+    }
+}
